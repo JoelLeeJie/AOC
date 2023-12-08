11fb42a54a3e74c7b4fb9c01ebf75b5ddaa06c09
Joel <onwingsofair@gmail.com>
2023-12-08 20:23:04 +0800

    Fixed Day 2 Part 2: adding +8 to string ptr did not skip past semicolon in Game 100:

--- 2023/2/game.c
+++ 2023/2/game.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
         sscanf(argv[i + 1], "%d", &colourMax[i]);
     }
 
-    FILE *readFile = fopen("input2.txt", "r");
+    FILE *readFile = fopen("input.txt", "r");
     if (readFile == NULL)
     {
         printf("Unable to open file");
@@ -44,6 +44,8 @@ int main(int argc, char *argv[])
         }
         sum += counter;
         cubeminsum = cubeminsum + inputColour_Max[0] * inputColour_Max[1] * inputColour_Max[2];
+        printf("%d %d %d\n\n", inputColour_Max[0], inputColour_Max[1], inputColour_Max[2]);
+        if((inputColour_Max[0] * inputColour_Max[1] * inputColour_Max[2])<=1) printf("sus\n");
     }
 
     fclose(readFile);
@@ -60,13 +62,14 @@ void ParseString(char *buffer, int inputArr[])
     int temp = 0;
     char tempStr[10], *colours[] = {"red", "green", "blue"};
 
-    buffer += 8; // increase ptr by 8 chars, to account for Game__:
+    while(*buffer++ != ':'); //move buffer past the semicolon, postfix ++ intended.
 
     // divide the entire string into individual sections, using ";" and ","
     tokenPtr = strtok(buffer, ",;");
     while (tokenPtr != NULL)
     {
         sscanf(tokenPtr, "%d %s", &temp, tempStr);
+        printf("%d ", temp);
         for (size_t i = 0; i < sizeof(colours) / 8; i++)
         {
             // if it matches that colour, and is more than maximum, store as the new maximum for that colour
@@ -75,4 +78,5 @@ void ParseString(char *buffer, int inputArr[])
         }
         tokenPtr = strtok(NULL, ",;");
     }
+    printf("\n");
 }
