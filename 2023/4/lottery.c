eccd121013335d113463dd0cb06ade2d890542af
Joel <onwingsofair@gmail.com>
2023-12-08 20:03:19 +0800

    Day 4 part 2 done

--- 2023/4/lottery.c
+++ 2023/4/lottery.c
@@ -4,7 +4,8 @@
 #include "math.h"
 
 int *GetArray(const char *srcLine, int *size);
-int CountPoints(const int *winArr, int sizeWin, const int *ownArr, int sizeOwn);
+int CountPoints(const int *winArr, int sizeWin, const int *ownArr, int sizeOwn, int *pointCount);
+long int CountTotalCard(int *cardArray, int numberCards);
 
 
 
@@ -18,8 +19,11 @@ int main(void)
     }
 
     char buffer[500] = {0};
-    int sum = 0;
+    int sum = 0, temp = 0;
     char secondBuffer[500] = {0};
+
+    int cardCounter = 0, currentAlloc = 8;
+    int *cardSum_DynamicArray = malloc(8);
     while(fgets(buffer, 500, readFile)!= NULL)
     {
         int *winDynamicArray, *ownDynamicArray;
@@ -34,12 +38,52 @@ int main(void)
         token = strtok(NULL, "|");
         ownDynamicArray = GetArray(token, &sizeOwnArr);
 
-        sum += CountPoints(winDynamicArray, sizeWinArr, ownDynamicArray, sizeOwnArr);
+
+        sum += CountPoints(winDynamicArray, sizeWinArr, ownDynamicArray, sizeOwnArr, &temp);
         free(winDynamicArray);
         free(ownDynamicArray);
+
+        cardCounter++;
+        if(currentAlloc<cardCounter*8) //not enough space to store all the cards currently.
+        {
+            cardSum_DynamicArray = realloc(cardSum_DynamicArray, currentAlloc * 2);
+            currentAlloc *= 2;
+        }
+        cardSum_DynamicArray[cardCounter-1] = temp;
     }
-    printf("%d", sum);
+    printf("%d\n", sum);
+    printf("%ld\n", CountTotalCard(cardSum_DynamicArray, cardCounter));
+    free(cardSum_DynamicArray);
     return 0;
+}
+
+long int CountTotalCard(int *cardArray, int numberCards)
+{
+    long int sum = 0, matchingNumbers = 0;
+    for(int i = 0; i<numberCards; i++)
+    {
+        printf("%d, ", cardArray[i]);
+    }
+    printf("\n");
+
+    //change array from back to front.
+    for(int i = numberCards-1; i>=0; --i)
+    {
+        //If card[5] has 2 matching, it'll add card[6] and card[7] to itself.
+        matchingNumbers = cardArray[i];
+        for(int j = 1; j<=matchingNumbers && j+i<numberCards; j++) //don't go out of bounds of array.
+        {
+            cardArray[i] += cardArray[i+j];
+        }
+    }
+
+    for(int i = 0; i<numberCards; i++)
+    {
+        sum+= cardArray[i]+1; //+1 to account for the original card too.
+        printf("%d, ", cardArray[i]);
+    }
+    printf("\n");
+    return sum;
 }
 
 int *GetArray(const char *srcLine, int *size)
@@ -67,7 +111,7 @@ int *GetArray(const char *srcLine, int *size)
 }
 
 
-int CountPoints(const int *winArr, int sizeWin, const int *ownArr, int sizeOwn)
+int CountPoints(const int *winArr, int sizeWin, const int *ownArr, int sizeOwn, int *pointCount)
 {
     int points = 0;
     for(int own = 0; own<sizeOwn; own++)
@@ -81,5 +125,6 @@ int CountPoints(const int *winArr, int sizeWin, const int *ownArr, int sizeOwn)
             }
         }
     }
+    *pointCount = points;
     return pow(2, points-1);
 }
